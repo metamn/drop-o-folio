5fdace9e08f7467d9041503b815210f39e3ffaf8
Your Name Here <bartus.csongor@gmail.com>
2012-11-14 22:33:13 +0200

    Sign up form added via the WP Members plugin

--- wp-content/themes/drop-o-folio/functions.php
+++ wp-content/themes/drop-o-folio/functions.php
@@ -1,5 +1,11 @@
 <?php
 	
-	
+	// User registration
+	// - remove "Required field" ... otherwise not possible via CSS
+	add_filter( 'wpmem_register_form', 'my_register_form_filter' );
+	function my_register_form_filter( $form ){
+		$form = str_replace("Required field", '', $form);
+		return $form;
+	}
 	
 ?>
